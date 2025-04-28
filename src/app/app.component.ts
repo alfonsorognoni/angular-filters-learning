fadc8e7e00a3e8ed2161eb4927bc25367ba01d3f
Alfonso Rognoni <alfonsorognoni@gmail.com>
2025-04-28 19:05:36 -0300

    learning directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,44 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MiComponenteComponent } from './mi-componente/mi-componente.component';
 import { User, UserCardComponent } from './user-card/user-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MiComponenteComponent, UserCardComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MiComponenteComponent,
+    UserCardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'filters';
   messageFromChild: string = '';
+  showTasks: boolean = false;
 
   users: User[] = [
     { name: 'John', email: 'john@example.com' },
     { name: 'Jane', email: 'jane@example.com' },
     { name: 'Bob', email: 'bob@example.com' },
+  ];
+
+  daysOfTheWeek: string[] = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  tasks: { id: number; description: string; done: boolean }[] = [
+    { id: 1, description: 'Aprender Componentes', done: true },
+    { id: 2, description: 'Aprender Directivas', done: false },
+    { id: 3, description: 'Practicar Data Binding', done: false },
   ];
 
   childEventHandler(messageFromChild: string) {
@@ -26,5 +49,9 @@ export class AppComponent {
 
   userSelectedHandler(userEmail: User['email']) {
     console.log(`User selected email: ${userEmail}`);
+  }
+
+  toggleShowTasks() {
+    this.showTasks = !this.showTasks;
   }
 }
--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-mi-componente',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mi-componente.component.html',
   styleUrl: './mi-componente.component.scss',
 })
@@ -13,6 +14,22 @@ export class MiComponenteComponent {
   title: string = 'Mi Componente';
   isDisabled: boolean = true;
   counter: number = 0;
+  showSecretMessage: boolean = false;
+  important: boolean = true;
+  hasError: boolean = true;
+  divClassNames: object = {
+    'text-xl': this.important,
+    'bg-error': this.hasError,
+    border: true,
+  };
+  textColor: string = 'green';
+  fontSize: number = 20;
+  paragraphStyle: object = {
+    color: this.textColor,
+    'font-size.px': this.fontSize,
+    'font-weight': 'bold',
+    'margin-top': '10px',
+  };
 
   ngOnInit(): void {
     console.log('Mi Componente is initialized ngOnInit');
@@ -28,5 +45,9 @@ export class MiComponenteComponent {
     this.counter++;
     const messageToParent = `The counter of ${this.title} has been incremented to ${this.counter}`;
     this.childEvent.emit(messageToParent);
+  }
+
+  toggleSecretMessage() {
+    this.showSecretMessage = !this.showSecretMessage;
   }
 }
